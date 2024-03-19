814e7a6623ade64a4b2e8437685c6d8e44938cb9
Simon "Tenry" Burchert <contact@simon-burchert.com>
2024-03-19 01:18:25 +0100

    Feature: Add wiki10.js to the template

---
+++ template/layout/wiki10.js
@@ -0,0 +1,70 @@
+(() => {
+  if (!window.wiki10 || !Array.isArray(window.wiki10.pages)) {
+    console.warn('window.wiki10.pages is not an array');
+    return;
+  }
+
+  const searchInput = document.getElementById('search');
+  const suggestionsBox = document.getElementById('suggestions');
+
+  if (!searchInput || !suggestionsBox) {
+    console.warn('missing #search or #suggestions');
+    return;
+  }
+
+  function toggleSuggestions(show) {
+    suggestionsBox.hidden = !show;
+  }
+
+  function setSuggestions(pages) {
+    const ul = suggestionsBox.querySelector('ul');
+    ul.textContent = '';
+
+    for (const page of pages) {
+      const li = ul.appendChild(document.createElement('li'));
+      const a = li.appendChild(document.createElement('a'));
+      a.href = page.filename;
+      a.textContent = page.title;
+    }
+  }
+
+  function findPages() {
+    const search = searchInput.value.replace(/\s+/, ' ').trim().toLowerCase();
+
+    if (search == '') {
+      toggleSuggestions(false);
+      return;
+    }
+
+    const pages = window.wiki10.pages.filter(page => {
+      return page.title.toLowerCase().includes(search);
+    });
+
+    if (pages.length > 0) {
+      setSuggestions(pages);
+      toggleSuggestions(true);
+    } else {
+      toggleSuggestions(false);
+    }
+  }
+
+  searchInput.addEventListener('focus', () => {
+    findPages();
+  });
+
+  searchInput.addEventListener('input', () => {
+    findPages();
+  });
+
+  searchInput.addEventListener('blur', event => {
+    if (!event.relatedTarget) {
+      suggestionsBox.hidden = true;
+    }
+
+    const parent = event.relatedTarget?.closest('#suggestions');
+
+    if (!parent) {
+      suggestionsBox.hidden = true;
+    }
+  });
+})();
